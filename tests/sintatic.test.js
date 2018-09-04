334e25fc082b4e85b331273a6bbd79625e946686
claudio <cdjohnnatha@gmail.com>
2018-09-04 15:38:21 -0300

    removed unused files to sintatic

--- tests/sintatic.test.js
+++ tests/sintatic.test.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const sintatic = require('../lib/Syntactic/sintatic');
+const sintatic = require('../lib/sintatic');
 
 test('test Lexicon', async () => {
   const filePath = path.join(__dirname, './', '/support/pascalFile.in');
